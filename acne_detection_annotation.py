4c36399cf7ee06e13f4ebe3087be7fae1ff4899f
Jeharul-Hussain <53990333+Jeharul-Hussain@users.noreply.github.com>
2020-09-30 23:40:03 +0530

    Update acne_detection_annotation.py

--- acne_detection_annotation.py
+++ acne_detection_annotation.py
@@ -16,7 +16,7 @@ import imageio
 os.chdir('Mask_RCNN')
 os.system('python setup.py install')
 print('before sleeping')
-time.sleep(30)
+time.sleep(10)
 print('after sleeping')
 os.chdir('..')
 from mrcnn.config import Config
